b0bd5995bbc5fd22024ad5428c7ea8fec10ef0fd
RyanBulluss <rbulluss2000@gmail.com>
2024-02-19 22:13:42 +0000

    chang routes and create gamePage component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react';
-import './App.css';
-import { getUser } from '../../utilities/users-service';
-import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar';
+import { useState } from "react";
+import "./App.css";
+import { getUser } from "../../utilities/users-service";
+import AuthPage from "../AuthPage/AuthPage";
+// import NewOrderPage from "../NewOrderPage/NewOrderPage";
+// import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import GamesPage from "../GamesPage";
+import NavBar from "../../components/NavBar/NavBar";
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   const [user, setUser] = useState(getUser());
-  
 
   return (
-    <main className='container'>
-      { user ? 
-        
-      <>
-      <NavBar user={user} setUser={setUser} />
-      <Routes>
-        <Route path='/orders/new' element={<NewOrderPage />} />
-        <Route path='/orders' element={<OrderHistoryPage />} />
-      </Routes> 
-      </>
-      : 
-      <AuthPage setUser={setUser} />
-      }
+    <main className="container">
+      {user ? (
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes>
+            <Route path="/" element={<GamesPage />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
     </main>
   );
 }
-
-
