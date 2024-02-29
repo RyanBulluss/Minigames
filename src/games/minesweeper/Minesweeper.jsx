23e7b6cb7e3faf8430dabd28d6235252e822f470
RyanBulluss <rbulluss2000@gmail.com>
2024-02-29 14:55:13 +0000

    Create Minesweeper controls and background

--- src/games/minesweeper/Minesweeper.jsx
+++ src/games/minesweeper/Minesweeper.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import MinesweeperControls from "./MinesweeperControls";
 
 const Minesweeper = () => {
   return (
-    <div>Minesweeper</div>
-  )
-}
+    <div className="h-full w-full flex flex-col">
+      <MinesweeperControls />
+      <div className="h-full flex justify-center items-center bg-gray-600">
+        <div
+          style={{
+            gridTemplateColumns: `repeat(2, minmax(0, 1fr))`,
+          }}
+          className="h-[70vmin] w-[80vmin] grid"
+        ></div>
+      </div>
+    </div>
+  );
+};
 
-export default Minesweeper
+export default Minesweeper;
