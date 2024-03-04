fd10474525c49154cafdbbeca5669d86de945417
RyanBulluss <rbulluss2000@gmail.com>
2024-03-04 14:55:33 +0000

    spawn random piece funcionality

--- src/games/tetris/Tetris.jsx
+++ src/games/tetris/Tetris.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TetrisControls from "./TetrisControls";
-import { board, createState, pieces } from "./constants";
+import { board, createState, getRandomPiece } from "./constants";
 import GameCell from "./GameCell";
 
 const Tetris = () => {
-    const [state, setState] = useState(createState);
+  const [state, setState] = useState(createState);
+
+  function spawnPiece() {
+    const newPiece = getRandomPiece();
+    setState((s) => {
+      const newState = JSON.parse(JSON.stringify(s));
+      let y = 0;
+      let x = 0;
+      newPiece.forEach((val, idx) => {
+        if (idx !== 0 && idx % 4 === 0) {
+          y++;
+          x = 0;
+        }
+        newState[y][x] = val;
+        x++;
+      });
+      return newState;
+    });
+  }
+
+  useEffect(() => {
+    spawnPiece();
+  }, [])
 
   return (
     <div className="h-full w-full flex flex-col">
@@ -16,9 +38,11 @@ const Tetris = () => {
           }}
           className="h-[70vmin] grid w-[35vmin]"
         >
-            {state.map((row, yIdx) => row.map((value, xIdx) => (
-                <GameCell key={yIdx + xIdx} value={value} />
-            )))}
+          {state.map((row, yIdx) =>
+            row.map((value, xIdx) => (
+              <GameCell key={yIdx + xIdx} value={value} />
+            ))
+          )}
         </div>
       </div>
     </div>
--- src/games/tetris/constants.js
+++ src/games/tetris/constants.js
@@ -15,50 +15,54 @@ function createState() {
     return arr;
 }
 
-const pieces = {
-    L: [
+const pieces = [
+    [
         0, 0, 1, 0,
         0, 0, 1, 0,
         0, 0, 1, 1, 
         0, 0, 0, 0, 
 
     ],
-    J: [
+    [
         0, 0, 2, 0, 
         0, 0, 2, 0, 
         0, 2, 2, 0, 
         0, 0, 0, 0,
     ],
-    T: [
+    [
         0, 0, 3, 0,
         0, 3, 3, 3, 
         0, 0, 0, 0, 
         0, 0, 0, 0, 
     ],
-    I: [
+    [
         0, 0, 4, 0,
         0, 0, 4, 0,
         0, 0, 4, 0,
         0, 0, 4, 0,
     ],
-    O: [
+    [
         0, 5, 5, 0,
         0, 5, 5, 0,
         0, 0, 0, 0,
         0, 0, 0, 0,
     ],
-    S: [
+    [
         0, 0, 6, 6,
         0, 6, 6, 0,
         0, 0, 0, 0,
         0, 0, 0, 0,
     ],
-    Z: [
+    [
         0, 7, 7, 0,
         0, 0, 7, 7,
         0, 0, 0, 0,
         0, 0, 0, 0,
     ],
+]
+
+function getRandomPiece() {
+    return pieces[Math.floor(Math.random() * pieces.length)];
 }
 
-export { board, createState, pieces }
+export { board, createState, getRandomPiece }
