4d9e07cc16d7112afce9119b6a372c6e14010ce3
RyanBulluss <rbulluss2000@gmail.com>
2024-03-15 00:15:42 +0000

    copy colors from official game

--- src/games/2048/GameCell.jsx
+++ src/games/2048/GameCell.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const GameCell = ({ value }) => {
   return (
-    <div style={{
-      border: "1px solid rgb(36, 35, 35)",
-      background: 
-      value === 2 ? "brown" : 
-      value === 4 ? "orange" : 
-      value === 8 ? "red" : 
-      value === 16 ? "blue" : ""
-    }}
-    className='flex justify-center items-center text-white font-bold text-2xl'
+    <div
+      style={{
+        border: "1px solid rgb(36, 35, 35)",
+        color: value === 2 || value === 4 ? "black" : "",
+        background:
+          value === 2
+            ? "#eee4da"
+            : value === 4
+            ? "#eee1c9"
+            : value === 8
+            ? "#f3b27a"
+            : value === 16
+            ? "#f69664"
+            : value === 32
+            ? "#f77c5f"
+            : value === 64
+            ? "#f75f3b"
+            : value === 128
+            ? "#edd073"
+            : value === 256
+            ? "#edcc62"
+            : value === 512
+            ? "#edc950"
+            : value === 1024
+            ? "#edc53f"
+            : "",
+      }}
+      className="flex justify-center items-center text-white font-bold text-2xl"
     >
-      {!!value && value }
+      {!!value && value}
     </div>
-  )
-}
+  );
+};
 
-export default GameCell
+export default GameCell;
