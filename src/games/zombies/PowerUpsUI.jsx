43bd94aa3ba14d1e8b7b6347b6522bcd281cacfe
RyanBulluss <rbulluss2000@gmail.com>
2024-04-08 21:26:23 +0100

    add speed up image

--- src/games/zombies/PowerUpsUI.jsx
+++ src/games/zombies/PowerUpsUI.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./Zombies.css"
 
 const PowerUpsUI = ({currentPowerUps, board}) => {
   return (
     <div
-    style={{position: "absolute", left: board.x, top: board.y, height: board.height / 10}}
-    className='flex gap-4'>
+    style={{position: "absolute", left: board.x, top: board.y, height: board.height / 10, width: board.width}}
+    className='flex gap-4 px-4'>
         {currentPowerUps.nuke && <div>Nuke</div>}
         {currentPowerUps.instantKill && <div>Instant Kill</div>}
         {currentPowerUps.doubleSpeed && <div>Double Speed</div>}
