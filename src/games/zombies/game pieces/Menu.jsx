f451d1792f78467d4afac8732843f486f5759013
RyanBulluss <rbulluss2000@gmail.com>
2024-04-17 21:36:59 +0100

    add icons to zom game

--- src/games/zombies/game pieces/Menu.jsx
+++ src/games/zombies/game pieces/Menu.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBinoculars,
+  faShieldAlt,
+  faCrosshairs,
+} from "@fortawesome/free-solid-svg-icons";
 
-
-const Menu = ({startGame, loadouts}) => {
-
-    const LoadoutButton = ({name}) => {
-        return (
-            <button 
-            className='hover:bg-slate-500/50 py-2 px-4 rounded-xl'
-            onClick={() => startGame(loadouts[name.toLowerCase()])}
-            >{name}</button>
-        )
-    }
+const Menu = ({ startGame, loadouts, icon }) => {
+  const LoadoutButton = ({ name, icon }) => {
+    return (
+      <button
+        className="hover:bg-slate-500/50 py-2 px-4 rounded-xl"
+        onClick={() => startGame(loadouts[name.toLowerCase()])}
+      >
+        {name}&nbsp;&nbsp;{icon}
+      </button>
+    );
+  };
 
   return (
     <div className="h-full w-full flex flex-col gap-4 justify-center items-center text-2xl">
-          <h2 className='font-semibold p-4'>Choose Your Loadout:</h2>
-          <LoadoutButton name="Scout" />
-          <LoadoutButton name="Sniper" />
-          <LoadoutButton name="Tank" />
-        </div>
-  )
-}
+      <h2 className="font-semibold p-4">Choose Your Loadout:</h2>
 
-export default Menu
+      <LoadoutButton
+        name="Scout"
+        icon={<FontAwesomeIcon icon={faBinoculars} />}
+      />
+
+      <LoadoutButton
+        name="Sniper"
+        icon={<FontAwesomeIcon icon={faCrosshairs} />}
+      />
+
+      <LoadoutButton
+        name="Tank"
+        icon={<FontAwesomeIcon icon={faShieldAlt} />}
+      />
+    </div>
+  );
+};
+
+export default Menu;
