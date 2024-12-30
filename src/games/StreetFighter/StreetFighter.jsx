4cc67566b8749d47c8f64392f09b64798f44f346
RyanBulluss <rbulluss2000@gmail.com>
2024-12-30 23:51:18 +0000

    Imports

--- src/games/StreetFighter/StreetFighter.jsx
+++ src/games/StreetFighter/StreetFighter.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
+
 const StreetFighter = ({ currentGame, user, setUpdateLb }) => {
+  const [state, setState] = useState([]);
+
   return (
     <div className="h-full w-full flex flex-col">
         <div className="h-full flex justify-center items-center bg-gray-500">
