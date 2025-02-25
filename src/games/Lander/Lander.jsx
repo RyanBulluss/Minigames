7f6898fe91bdc856f2e5c0bc8d66807784f48811
RyanBulluss <rbulluss2000@gmail.com>
2025-02-25 16:12:26 +0000

    start rotation

--- src/games/Lander/Lander.jsx
+++ src/games/Lander/Lander.jsx
@@ -3,11 +3,41 @@ import Rocket from "./Rocket";
 
 const Lander = ({ currentGame, user, setUpdateLb }) => {
   const [state, setState] = useState([]);
+  const [rocket, setRocket] = useState({angle: 0})
+
+
+  useEffect(() => {
+
+
+
+    const handleKeyPress = (e) => {
+      e.preventDefault();
+      if (e.key === "a") {
+        setRocket(r => {
+          return {...r, angle: r.angle - 10}
+        })
+      }
+      if (e.key === "d") {
+        setRocket(r => {
+          return {...r, angle: r.angle + 10}
+        })
+      }
+    };
+
+
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
 
   return (
     <div className="h-full w-full flex flex-col">
         <div className="h-full flex justify-center items-center bg-gray-500">
-          <Rocket />
+          <Rocket rocket={rocket} />
+          {rocket.angle}
         </div>
     </div>
   );
--- src/games/Lander/Rocket.jsx
+++ src/games/Lander/Rocket.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const Rocket = () => {
+const Rocket = ({ rocket }) => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-    <div className='h-7 w-7 bg-black rotate-45 mb-[-13px]'></div>
-    <div className='h-20 w-10 bg-black'></div>
+    <div className='flex flex-col justify-center items-center'
+    style={{ rotate: `${rocket.angle}deg` }}
+    >
+      <div className='h-7 w-7 bg-black rotate-45 mb-[-13px]'></div>
+      <div className='h-20 w-10 bg-black'></div>
+
+      <div className='flex'>
+      <div className='h-3 w-3 rotate-45 mt-[-5px] bg-black'></div>
+      <div className='h-3 w-3 rotate-45 mt-[-5px] bg-black'></div>
+      <div className='h-3 w-3 rotate-45 mt-[-5px] bg-black'></div>
+
+         
+      </div>
+
     </div>
   )
 }
